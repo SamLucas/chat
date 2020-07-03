a14257165d6080623691332185d63bb6e38326af
Samuel Lucas <samuellucas0603@gmail.com>
2020-07-03 20:39:17 -0300

    feature: chat em grupo.

--- server.js
+++ server.js
@@ -6,12 +6,26 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "public"));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 
 app.use("/", (req, res) => {
   res.render("index.html");
+});
+
+let messages = [];
+
+io.on("connection", (socket) => {
+  console.log(`Socket conectado: ${socket.id}`);
+
+  socket.emit("previusMenssage", messages);
+
+  socket.on("sendMessage", (data) => {
+    messages.push(data);
+    socket.broadcast.emit("receivedMessage", data);
+    console.log(data);
+  });
 });
 
 server.listen(3000);
